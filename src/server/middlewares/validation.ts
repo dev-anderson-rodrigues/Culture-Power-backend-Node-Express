11124d08e7c3a9759e80f193c814ac3b241f2f60
anderson <andersoncassio2008@gmail.com>
2024-02-16 21:49:13 -0300

    add commit

--- src/server/middlewares/validation.ts
+++ src/server/middlewares/validation.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { type RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { type Schema, type ValidationError } from 'yup'
