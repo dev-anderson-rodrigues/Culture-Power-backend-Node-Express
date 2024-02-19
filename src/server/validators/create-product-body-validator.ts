6a37e72ccd07cdd7d3385971fe6b288bb68de9f9
anderson <andersoncassio2008@gmail.com>
2024-02-18 22:34:32 -0300

    commit details finish project

--- src/server/validators/create-product-body-validator.ts
+++ src/server/validators/create-product-body-validator.ts
@@ -2,8 +2,8 @@ import * as yup from 'yup'
 import { type IProductDTO } from '../entities/product'
 
 export const createProductBodySchema = yup.object<IProductDTO>({
-  name: yup.string().required(),
-  value: yup.number().required(),
-  amount: yup.number().required(),
-  description: yup.string().required()
+  name: yup.string().required('name is required!'),
+  value: yup.number().required('value is required!'),
+  amount: yup.number().required('amount is required!'),
+  description: yup.string().required('description is required!')
 })
